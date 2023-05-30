3b361ac9dc3565f0f7e8a8181bdd98548460711a
Matheus <matheus_dario@outlook.com>
2023-05-30 15:47:19 -0300

    feat: :sparkles: created first command

--- index.js
+++ index.js
@@ -30,13 +30,28 @@ for (const file of commandFiles) {
   }
 }
 
-console.log(client.commands);
-
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+// Login do bot
 client.once(Events.ClientReady, (c) => {
   console.log(`Login realizado com ${c.user.tag}`);
 });
 
-// Log in to Discord with your client's token
 client.login(TOKEN);
+
+// Listener de interações com o bot
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error("Comando não encontrado");
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply("Houve um erro ao executar esse comando");
+  }
+});
