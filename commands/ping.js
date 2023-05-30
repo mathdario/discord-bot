3b35e1ef900a3d2172dc25e05c7b3aad34b72913
Matheus <matheus_dario@outlook.com>
2023-05-30 14:13:05 -0300

    feat: :sparkles: setting commands routes

---
+++ commands/ping.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = requere("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Responde com 'Pong!"),
+
+  async execute(interaction) {
+    await interaction.reply("Pong!");
+  },
+};
--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const dotenv = require("dotenv");
 dotenv.config();
 const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
 
+//importações dos comandos
+const fs = require("node:fs");
+const path = require("node:path");
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const commands = require(filePath);
+}
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // When the client is ready, run this code (only once)
